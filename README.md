e3e04ac3a722c10bcfbb0d6c7127a299753c04ff
Christofer Peterson <cpeterson@healthgrades.com>
2019-09-13 07:48:08 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# gridcannon
-a solitaire game
+# Gridcannon
+
+This project's goal is to recreate the solitaire game Gridcannon. The work on this project was inspired by the youtube video [How to play Gridcannon: a single player game with regular playing cards](https://www.youtube.com/watch?v=gqmUpQjFHrA) by [Tom Francis](https://www.youtube.com/channel/UCQ-twzO6v-PBgckhkrXVaDQ). Gridcannon is a single player card game with the goal of elimintating the royal cards with the use of a cannon made up of a grid of cards.
+
+## How to Play
+
+### The Deck
+Start with a well shuffled deck of cards that contains the cards 2-9, K, Q, J, A in each of the four suits and two joker cards. In total there should be 54 cards in the deck: 13 Hearts, 13 Diamonds, 13 Clubs, 13 Spades, and two jokers.
+
+### The Deal
+
+Keeping the deck face down, deal cards face up in a three by three grid leaving the central tile empty. If any royal (J, Q, K) cards are dealt set those aside in a separate, face down pile. Stop dealing when the grid is complete.
+
+#### The Royals
+
+Once the grid is complete, retrive the set aside deck of royals and shuffle it, keeping it face down. Deal the first royal to outside of the grid. Royals must be placed next to the highest card of that suit or color. For example, a J of hearts would be placed next to a 5 of hearts. A K of spades would be placed next to a 3 of clubs if there are no spades. A Q of clubs would be placed next to a 7 of diamonds if there are no spades, clubs, or higher diamonds or hearts. When being placed on a corner, choose which side the card should be on. See the killing royals section to formulate strategy. Once a space is occupied by a royal, it cannot be removed or replaced.
+
+In the end, the board should look like this:
+
+|   |   |   |   |   |  
+|---|---|---|---|---|
+| X | R | R | R | X |
+| R | C | C | C | R |
+| R | C | F | C | R |
+| R | C | C | C | R |
+| X | R | R | R | X |
+
+Where X is an unplayable tile, R is where a royal would be, C is where regular cards should be, and F is a free space.
+
+### The Round
+
+Once the grid is placed and all set aside royals are dealt, rounds can begin. A round begins by taking the top card from the remaining deck. There are three types of cards that can be drawn that have special rules: Royals, Cannon Fodder, or Resets.
+
+### Royals
+
+Drawing a royal during a round is handled just as it was during the deal. The royal is placed outside of the grid on a free space next to the card most similar to it in the grid. See "The Royals" section in "The Deal" for more details.
+
+### Cannon Fodder
+
+Most cards will be cannon fodder. Cards with a face value of 2-10 are cannon fodder and must be placed on the grid. A card can only be placed on a tile that has a value lower than it. For example, a 7 can be placed on a 6 but it cannot be placed on a 8. The suits are irrelevant to the placement of the cards.
+
+If a card is drawn that cannot be placed on the grid, the player suffers a choice of penalty. The first choice is the card can become armor for a royal. The armor must first match the suit of the royal it is applying to. If a royal of matching suit is not available, the player can choose which royal to apply the armor. The second choice is to add the card to the shame pile. Place the card out of play in a separate deck. Choose a grid pile to reset by shuffling it and placing it at the bottom of your main deck. The grid tile is now a free space.
+
+With the exception of the outside grid, placing a card will trigger a cannon to go off. The cannon will fire orthoginally from the placed card toward the royal on the edge. This is how royals are killed. See the "Killing Royals" section for details on how Royals are killed.
+
+#### Killing Royals
+
+Royals are killed by adding up the two cannon fodder cards closest to the royal on the same line. Royals are killed (turned face down) when they occupy a tile outside of the grid that was fired on by a cannon (see "Cannon Fodder") and the sum total of two cards closest to the Royal is greater than or equal to the value of the Royal plus any armor they have acquired. Jacks have 11 life and can be killed by cannon fodder of any suit. Queens have 12 life and can be killed by cannon fodder of the same color (e.g. a queen of spades can only be killed by clubs or spades). Kings have 13 life and can be killed by cannon fodder of the same suit.
+
+### End of Game
+
+The game is over when the player successfully kills all of the Royals. The player earns 11 points for each Jack, 12 points for each Queen, and 13 points for each King for a total of 144 points. The player loses a point each round of play where a Royal is not placed. The goal is to finish the game with as many points as possible. If the player ends with 0 or less points, he or she has lost.
---
+++ gridcannon.js
@@ -0,0 +1,209 @@
+const script = document.currentScript;
+const canvas = document.createElement("canvas");
+const cardSprite = document.createElement("img");
+var newImg = new Image;
+newImg.onload = function() {
+    cardSprite.src = this.src;
+}
+newImg.src = 'src/cards.gif';
+
+const FPS = 30;
+const NUMBER_OF_JOKERS = 2;
+const GRID_SIZE = 3;
+const CARD_HEIGHT = 58.5;
+const CARD_WIDTH = 40.5;
+const TILE_Y_OFFSET = CARD_HEIGHT + 2;
+const TILE_X_OFFSET = CARD_WIDTH + 2;
+const DEFAULT_BUTTON_PADDING = 30;
+
+const Gridcannon = function(context) {
+    this.context = context;
+    this.context.canvas.height = 480;
+    this.context.canvas.width = 860;
+    this.assets = Gridcannon.BuildUI();
+    this.frames = 1;
+}
+
+Gridcannon.prototype.run = function() {
+    this.assets.push(this.gameboard = this.buildGameBoard());
+    this.deck = this.buildDeck(NUMBER_OF_JOKERS);
+    this.deck = this.shuffle(this.deck);
+    this.shameDeck = [];
+    this.gameLoopId = setInterval(() => this.loop(), 1000 / FPS);
+}
+
+Gridcannon.prototype.loop = function() {
+    this.process();
+    this.update();
+    this.draw(this.context);
+}
+
+Gridcannon.prototype.process = function() {
+    
+}
+
+Gridcannon.prototype.update = function() {
+    this.assets.forEach(asset => !!asset.update && asset.update());
+}
+
+Gridcannon.prototype.draw = function(ctx) {
+    this.assets.forEach(asset => !!asset.draw && asset.draw(ctx));
+}
+
+Gridcannon.prototype.buildDeck = (numberOfJokers) => {
+    const output = [];
+
+    for (let i = 1; i < 53; i++) {
+        var modulo = i%13;
+        var suit = Math.ceil(i/13);
+        var card;
+        switch (modulo) {
+            case 1: // aces
+                card = new Card('A', modulo, suit);
+                break;
+            case 11: // jacks
+                card = new Card('J', 11, suit);
+                break;
+            case 12: // queens
+                card = new Card('Q', 12, suit);
+                break;
+            case 0: // kings
+                card = new Card('K', 13, suit);
+                break;
+            default: // cannon fodder cards
+                card = new Card(`${modulo}`, modulo, suit)
+                break;
+        }
+        output.push(card);
+    }
+
+    // add the jokers
+    for (let i = 0; i < numberOfJokers; i++) {
+        output.push(new Card('0', 0, 0))        
+    }
+
+    return output;
+}
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+Gridcannon.prototype.shuffle = (a) => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+Gridcannon.prototype.buildGameBoard = () => {
+    const tiles = [];
+
+    if(GRID_SIZE % 1 !== 0) {
+        throw 'The grid size must be an odd number.';
+    }
+
+    const totalGridSize = GRID_SIZE + 2;
+    for (let i = 0; i < totalGridSize; i++) {
+        tiles[i] = [];
+        for(let j = 0; j < totalGridSize; j++) {
+            const isBlocked = (i == 0 || i == totalGridSize - 1) && (j == 0 || j == totalGridSize - 1);
+            const isRoyalsOnly = !isBlocked && ((i == 0 || i == totalGridSize - 1 ) || (j == 0 || j == totalGridSize - 1));
+            const isTrigger = !isRoyalsOnly && !isBlocked && ((i == 1 || i == totalGridSize - 2) || (j == 1 || j == totalGridSize - 2));
+            tiles[i][j] = new Tile(isBlocked, isRoyalsOnly, isTrigger);
+        }
+    }
+
+    return new Gameboard(tiles);
+}
+
+Gridcannon.BuildUI = () => {
+    const output = [];
+    
+    output.push(new Background());
+    output.push(new Button("New Game", 25, 25, DEFAULT_BUTTON_PADDING, () => {}));
+
+    return output;
+}
+
+const Gameboard = function(tiles) {
+    this.tiles = tiles;
+}
+
+Gameboard.prototype.draw = function(ctx) {
+    let y = (ctx.canvas.height / 2) - ((GRID_SIZE + 2) * TILE_Y_OFFSET / 2);
+    let x = (ctx.canvas.width / 2) - ((GRID_SIZE + 2) * TILE_X_OFFSET / 2);
+
+    for (let i = 0; i < this.tiles.length; i++) {
+        const row = this.tiles[i];
+        for (let j = 0; j < row.length; j++) {
+            const tile = row[j];
+            tile.y = y + (i * TILE_Y_OFFSET);
+            tile.x = x + (j * TILE_X_OFFSET);
+            tile.draw(ctx);
+        }
+    }
+}
+
+const Tile = function(blocked, royalsOnly, trigger) {
+    this.isBlocked = blocked; // indicates that this tile cannot be used
+    this.isRoyalsOnly = royalsOnly; // indicates this is a tile where royals must live
+    this.isTrigger = trigger; // indicates this tile triggers cannons
+    this.cards = [];
+}
+
+Tile.prototype.draw = function(ctx) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, CARD_WIDTH + 2, CARD_HEIGHT + 2);
+}
+
+const Card = function(type, strength, suit) {
+    this.type = type;
+    this.strength = strength;
+    this.suit = suit;
+}
+
+Card.prototype.draw = function(ctx) {
+
+}
+
+const Background = function() {
+
+}
+
+Background.prototype.draw = function(ctx) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+const Button = function(text, x, y, padding, onClick) {
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    this.onClick = onClick;
+    this.padding = padding;
+}
+
+Button.prototype.update = function() {
+
+}
+
+Button.prototype.draw = function(ctx) {
+    ctx.font = '20px Kremlin Pro Web';
+    ctx.textAlign="center";
+    ctx.textBaseline = "middle";
+    const measure = ctx.measureText(this.text);
+    this.height = 20 + this.padding;
+    this.width = measure.width + this.padding;
+    ctx.fillStyle = '#FFFFFF'; 
+    ctx.fillStyle = 'rgba(225,225,225,0.5)';
+    console.log(this);
+    ctx.fillRect(this.x, this.y, this.width, this.height); 
+    ctx.fillStyle = `rgba(255,255,255,1)`;
+    ctx.fillText(this.text, this.x + this.width/2, this.y + this.height/2);
+}
+
+script.before(canvas);
+const gridcannon = new Gridcannon(canvas.getContext("2d"));
+gridcannon.run();
